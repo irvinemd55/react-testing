41307ababdf91d37532c055e6a2399d014999166
irvinemd55 <irvinedowning@gmail.com>
2017-05-06 18:02:54 -0700

    day finished

--- js/Details.js
+++ js/Details.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Header from './Header'
+
 const { shape, string } = React.PropTypes
 
 const Details = React.createClass({
@@ -15,13 +17,12 @@ const Details = React.createClass({
     const { title, description, year, poster, trailer } = this.props.show
     return (
       <div className='details'>
-        <header>
-          <h1>svideo</h1>
-        </header>
+        <Header />
         <section>
           <h1>{title}</h1>
           <h2>({year})</h2>
-          <img src={`/public/img/posers/${poster}`} />
+          <img src={`/public/img/posters/${poster}`} />
+          <p>{description}</p>
         </section>
         <div>
           <iframe src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`} frameBorder='0' allowFullScreen />
