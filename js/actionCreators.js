60891521274074d086099aaacc626790e66d6086
irvinemd55 <irvinedowning@gmail.com>
2017-05-10 17:58:07 -0700

    redux-async

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -1,4 +1,5 @@
 import { SET_SEARCH_TERM, ADD_OMDB_DATA } from './actions'
+import axios from 'axios'
 
 export function setSearchTerm (searchTerm) {
   return { type: SET_SEARCH_TERM, searchTerm }
@@ -6,4 +7,14 @@ export function setSearchTerm (searchTerm) {
 
 export function addOMDBData (imdbID, omdbData) {
   return { type: ADD_OMDB_DATA, imdbID, omdbData }
+}
+
+export function getOMDBDetails (imdbID) {
+  return function (dispatch, getState) {
+    axios.get(`http://www.omdbapi.com/?i=${imdbID}`)
+    .then((response) => {
+      dispatch(addOMDBData(imdbID, response.data))
+    })
+    .catch((error) => console.error('axios error', error))
+  }
 }
